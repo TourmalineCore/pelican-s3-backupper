43bb6f18ac55f114ca8415c279e2004a0a823954
Yam1x <rychkov03@yandex.ru>
2025-03-26 15:22:29 +0500

    chore: create directory for backup and variable for directory path

--- s3-backup.py
+++ s3-backup.py
@@ -7,16 +7,19 @@ from datetime import datetime
 def main():
 
     cfg_path = r'.rclone-test.conf'
+    directory_path = "/tmp/s3-backup"
+
+    os.mkdir(directory_path, mode=0o777, *, dir_fd=None)
 
     with open(cfg_path) as f:
         cfg = f.read()
 
     archive_name = "s3-backup" + '_' + datetime.strftime(datetime.utcnow(), "%Y.%m.%d.%H-%M-%S") + 'UTC' + '.backup'
-
+    
     # s3-1:pelican-local-env is source
-    rclone.with_config(cfg).copy("s3-1:pelican-local-env", "/tmp/s3-backup", flags=["--transfers=256"])
+    rclone.with_config(cfg).copy("s3-1:pelican-local-env", directory_path, flags=["--transfers=256"])
 
-    shutil.make_archive(archive_name, 'zip', "/tmp/s3-backup")
+    shutil.make_archive(archive_name, 'zip', directory_path)
 
     # s3-2:backup is destination
     rclone.with_config(cfg).copy(archive_name + ".zip", "s3-2:backup", flags=["--transfers=256"])
