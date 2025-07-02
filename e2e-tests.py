17b8e024c2871f2cd3a8a7834ded1b37e88370a0
Yam1x <rychkov03@yandex.ru>
2025-07-02 16:05:08 +0500

    feat: check backup content

--- e2e-tests.py
+++ e2e-tests.py
@@ -25,10 +25,37 @@ def main():
             backups.append(content)
 
     if len(backups) == 0:
-        raise Exception("No database backups found")
+        raise Exception("No S3 backups found")
+    
+    latest_backup = backups[-1]
 
-    if backups[-1]["Size"] == 0:
+    if latest_backup["Size"] == 0:
         raise Exception("Backup size is 0")
+
+    s3.download_file(Bucket=os.getenv('S3_AWS_BUCKET_NAME'), Key=latest_backup['Key'], Filename=latest_backup['Key'])
+    file_names = []
+
+    try:
+        with zipfile.ZipFile(latest_backup["Key"], 'r') as zip_ref:
+            file_names = zip_ref.namelist()
+
+            if "test.txt" in file_names:
+                zip_ref.extract("test.txt")
+            else:
+                raise Exception("Test file not found")
+    except:
+        raise Exception("Error while reading zip file")
+
+    try:
+        with open("test.txt", "r") as test_file:
+            test_file_content = test_file.read()
+    except:
+        raise Exception("Error while reading test file")
+
+    if not "test" in test_file_content:
+        raise Exception("Test file content error. Test data is missing")
+
+    print(file_names)
 
 if __name__ == '__main__':
     main()
